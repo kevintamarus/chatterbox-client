5430d2a2f8365315d35e0815d6920337202b9fa7
Hack Reactor Student <student@hackreactor.com>
2017-07-07 15:02:00 -0700

    Implmented app.send/working on fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,47 @@
 // YOUR CODE HERE:
+$(document).ready(function() {
+
+  $('#post-message').on('click', function() {
+    let message = $('#message').val();
+    let userName = window.location.search.slice(10);
+
+    $('<div/>')
+      .text(message)
+      .prependTo('#chats');
+    
+    var data = {
+      username: userName,
+      text: message,
+      roomname: 'lobby'
+    };
+      
+    app.send(data);
+    
+    $('#message').val('');
+  });
+});
+
+let app = {};
+
+app.fetch = function() {
+  $.ajax({
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: (data) => console.log(data),
+    error: () => console.log('error')
+  });
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    data: JSON.stringify(message),
+    type: 'POST',
+    contentType: 'application/json',
+    success: () => console.log('message sent'),
+    error: () => console.log('message not sent')
+  });
+};
+
+app.init = function() {};
